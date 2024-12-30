dc648ccd581846d260515ff250c77ad15cacd803
AFatCat03 <AFatCat03@gmail.com>
2024-12-30 23:21:59 +0800

    chore: remove unnecessary #include <string.h> in detab.c

--- Chapter5/Section10/Exercise5-11/detab.c
+++ Chapter5/Section10/Exercise5-11/detab.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #define TAB_STOP 8
 /* replaces tabs in the input with the proper number of blanks to space to the next tab stop. */
 int main(int argc, char *argv[])
